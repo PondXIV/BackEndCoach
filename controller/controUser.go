5f182befa5adaf3c05dd6a6ed0a577b37fb08c1b
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-01-13 15:04:16 +0700

    updateLoginFB

--- controller/controUser.go
+++ controller/controUser.go
@@ -83,7 +83,9 @@ func loginFBPostBody(ctx *gin.Context) {
 
 	if coach.Cid > 0 {
 		ctx.JSON(http.StatusOK, models.Coach{Cid: coach.Cid})
+		ctx.JSON(http.StatusOK, models.Customer{Uid: cus.Uid})
 	} else if cus.Uid > 0 {
+		ctx.JSON(http.StatusOK, models.Coach{Cid: coach.Cid})
 		ctx.JSON(http.StatusOK, models.Customer{Uid: cus.Uid})
 	} else if cus.Uid == 0 {
 

e3cd5671e8d0708d2d209be93d08e87302a2b1dc
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-03-17 17:42:59 +0700

    commit

--- repository/repoCourse.go
+++ repository/repoCourse.go
@@ -23,7 +23,7 @@ var courses = []models.Course{}
 
 // GetCoachByCoID implements CourseRepository
 func (c courseDB) GetCouseByCoID(CoID int) (*models.Course, error) {
-
+	course := models.Course{}
 	result := c.db.Where("coID = ?", CoID).Find(&course)
 	if result.Error != nil {
 		return nil, result.Error
@@ -33,7 +33,7 @@ func (c courseDB) GetCouseByCoID(CoID int) (*models.Course, error) {
 
 // GetCouseByname implements CourseRepository
 func (c courseDB) GetCouseByname(Name string) (*[]models.Course, error) {
-
+	courses := []models.Course{}
 	result := c.db.Where("name like ?", Name+"%").Find(&courses)
 	if result.Error != nil {
 		return nil, result.Error
@@ -56,7 +56,7 @@ func (c courseDB) UpdateStatusCourse(CoID int, Status string) int64 {
 
 // GetCourseByIDCoach implements CourseRepository
 func (c courseDB) GetCourseByIDCoach(Cid int) (*[]models.Course, error) {
-
+	courses := []models.Course{}
 	result := c.db.Where("cid = ?", Cid).Where("bid IS NULL").Find(&courses)
 	if result.Error != nil {
 		return nil, result.Error
@@ -66,7 +66,7 @@ func (c courseDB) GetCourseByIDCoach(Cid int) (*[]models.Course, error) {
 
 // GetCourseAll implements CourseRepository
 func (c courseDB) GetCourseAll() (*[]models.Course, error) {
-
+	courses := []models.Course{}
 	result := c.db.Preload("Coach").Find(&courses)
 	if result.Error != nil {
 		return nil, result.Error

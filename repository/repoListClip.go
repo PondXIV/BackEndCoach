c167138ba502f2f7f7204162c032a41a28c16772
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-04-28 16:35:46 +0700

    commit

--- repository/repoListClip.go
+++ repository/repoListClip.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"backEndGo/models"
+
+	"gorm.io/gorm"
+)
+
+type ListClipRepository interface {
+	GetListClipByIDCoach(Cid int) (*[]models.ListClip, error)
+}
+type ListClipDB struct {
+	db *gorm.DB
+}
+
+func NewListClipRepository() ListFoodRepository {
+	db, err := NewDatabaseConnection()
+	if err != nil {
+		return nil
+	}
+
+	return LisFoodDB{db}
+}

a893ad8051eca6696646006cc709441e26f132cd
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-05-04 17:10:37 +0700

    updateRest_Api

--- controller/UserController/ctx_User.go
+++ controller/UserController/ctx_User.go
@@ -9,36 +9,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var nameCoachDateService = userservice.NewCoachByNameDataService()
-var reviewDataService = userservice.NewReviewDataService()
 var customerService = userservice.NewUserDataService()
 var updatecustomerService = userservice.NewUserDataService()
 var modelsCustomer = models.Customer{}
-var mycourseService = userservice.NewMyCourseDataService()
 
-func NewCourseController(router *gin.Engine) {
+func NewUserController(router *gin.Engine) {
 	nameCoach := router.Group("/user")
 	{
 		//เอา repo GetCourse ของเค้าได้
-		nameCoach.GET("/nameCourse/:name", GetCoachByName)
-		nameCoach.GET("/review/courseID/:coID", GetReviewByCoID)
+		// nameCoach.GET("/nameCourse/:name", GetCoachByName)
+		//nameCoach.GET("/review/courseID/:coID", GetReviewByCoID)
 		nameCoach.GET("/customerID/:uid", Customer)
 		nameCoach.PUT("/customerID/:uid", updateCustomer)
 		//ฟ่ด repo GetCourse ของเค้า bid IS NULL นะ ต้องเอาอันอืน
-		nameCoach.GET("/course/customerID/:uid", GetMycourse)
+		// nameCoach.GET("/course/customerID/:uid", GetMycourse)
 
 	}
 
 }
-func GetMycourse(ctx *gin.Context) {
-	cusID := ctx.Param("uid")
-	uid, err := strconv.Atoi(cusID)
-	course, err := mycourseService.ServiceGetMycourse(uid)
-	if err != nil {
-		panic(err)
-	}
-	ctx.JSON(http.StatusOK, course)
-}
+
 func updateCustomer(ctx *gin.Context) {
 	cusID := ctx.Param("uid")
 	uid, _ := strconv.Atoi(cusID)
@@ -62,16 +51,6 @@ func updateCustomer(ctx *gin.Context) {
 	}
 }
 
-func GetCoachByName(ctx *gin.Context) {
-	name := ctx.Param("name")
-	coachs, err := nameCoachDateService.ServiceGetNameCoach(name)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx.JSON(http.StatusOK, coachs)
-
-}
 func Customer(ctx *gin.Context) {
 	cusID := ctx.Param("uid")
 	uid, err := strconv.Atoi(cusID)
@@ -81,16 +60,4 @@ func Customer(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, customer)
-}
-func GetReviewByCoID(ctx *gin.Context) {
-
-	courseID := ctx.Param("coID")
-	coID, err := strconv.Atoi(courseID)
-	review, err := reviewDataService.ServiceGetReviewByCoId(coID)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx.JSON(http.StatusOK, review)
-
 }

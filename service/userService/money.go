90eaffeff74af8b2a47014c910d6b94988a53c17
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-06-19 02:54:09 +0700

    update

--- service/userService/money.go
+++ service/userService/money.go
@@ -44,24 +44,28 @@ func (g GbprimeData) ServiceWallet(ReferenceNo string, ResGb *models.Gbprimpay) 
 		return -1, err
 	}
 	if RowsAffected > 0 {
+		modelwallet, errs := repoWallet.GetUser(ReferenceNo)
+		if errs != nil {
+			return -1, err
+		}
+		if modelwallet.CustomerID != 0 {
+			User, _ := repoCus.UserByUid(modelwallet.CustomerID)
+			sum := User.Price + (modelwallet.Money * 1000.00)
+			rowsAffected, errs := repoWallet.UpdateWalletUid(int(User.Uid), sum)
+			fmt.Println("Price", User.Price, "///", ResGb.Amount, "///", modelwallet.Money)
+			if errs != nil {
+				return -1, err
+			}
+			return rowsAffected, nil
+		}
 		return 1, nil
+
 	} else if RowsAffected == 0 {
 		return 0, nil
 	} else {
 		return -1, nil
 	}
-	getcusID, errs := repoWallet.GetUser(ReferenceNo)
-	if errs != nil {
-		return -1, errs
-	}
-	User, _ := repoCus.UserByUid(getcusID.CustomerID)
-	rowsAffected, errs := repoWallet.UpdateWalletUid(int(User.Uid), (getcusID.Money)*5000)
-	fmt.Println("Price", User.Price, "///", ResGb.Amount)
 
-	if errs != nil {
-		return -1, err
-	}
-	return rowsAffected, nil
 }
 
 // ServiceGbprime implements GbprimeService.

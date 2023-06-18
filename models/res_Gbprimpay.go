d2b2d21f7dd6f03897e5829f200311d7dc3ad276
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-06-18 16:27:23 +0700

    update

--- models/res_Gbprimpay.go
+++ models/res_Gbprimpay.go
@@ -1,13 +1,13 @@
 package models
 
 type Gbprimpay struct {
-	Amount         int64   `json:"amount"`
+	Amount         float64 `json:"amount"`
 	RetryFlag      string  `json:"retryFlag"`
 	ReferenceNo    string  `json:"referenceNo"`
 	GbpReferenceNo string  `json:"gbpReferenceNo"`
 	CurrencyCode   string  `json:"currencyCode"`
 	ResultCode     string  `json:"resultCode"`
-	TotalAmount    int64   `json:"totalAmount"`
+	TotalAmount    float64 `json:"totalAmount"`
 	Fee            float64 `json:"fee"`
 	Vat            float64 `json:"vat"`
 	ThbAmount      int64   `json:"thbAmount"`

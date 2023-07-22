098e0a6faca8d5fe45911c0c6d3240db0bcf23f8
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-07-22 23:18:56 +0700

    update

--- service/userService/Review.go
+++ service/userService/Review.go
@@ -10,24 +10,15 @@ var repoReview = repository.NewReviewRepository()
 
 type ShowReviewService interface {
 	ServiceGetReviewByCoId(CoID int) (*[]models.Review, error)
-	ServiceInsertReview(CoID int, review *models.Review) (int64, error)
+	ServiceInsertReview(Bid int, review *models.Review) (int64, error)
 }
 type ReviewData struct {
 }
 
 // ServiceInsertReview implements ShowReviewService.
-func (ReviewData) ServiceInsertReview(CoID int, review *models.Review) (int64, error) {
-	//1
-	repo := repository.NewCourseRepository()
-	courseDup, err := repo.GetCouseByCoID(CoID)
-	fmt.Println(courseDup.CoID)
-
-	//2
-	billID, err := repo.GetCouseByCoID(int(courseDup.CoID))
-	fmt.Println(billID.BuyingID)
-	//3
+func (ReviewData) ServiceInsertReview(Bid int, review *models.Review) (int64, error) {
 	repobill := repository.NewBuyingRepository()
-	courseId, err := repobill.GetBuyingrAll(0, 0, int(billID.BuyingID), 0)
+	courseId, err := repobill.GetBuyingrAll(0, 0, Bid, 0)
 	for _, valuecourseId := range *courseId {
 
 		rowsAffecteds, _ := repoReview.InsertReview(int(valuecourseId.CourseID), review)
@@ -38,7 +29,7 @@ func (ReviewData) ServiceInsertReview(CoID int, review *models.Review) (int64, e
 	if err != nil {
 		panic(err)
 	}
-	return int64(billID.BuyingID), nil
+	return int64(Bid), nil
 }
 
 func (ReviewData) ServiceGetReviewByCoId(CoID int) (*[]models.Review, error) {
